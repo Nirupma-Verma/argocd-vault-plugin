701f22b7c06c86b17f1e53498a3719cda9b3fb59
Nirupma Verma <nirupma.verma@cyberark.com>
2025-09-03 12:06:31 +0530

    unit testcases

--- pkg/backends/cyberarksecretsmanager.go
+++ pkg/backends/cyberarksecretsmanager.go
@@ -2,20 +2,27 @@ package backends
 
 import (
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 
-	"github.com/cyberark/conjur-api-go/conjurapi"
 	"github.com/argoproj-labs/argocd-vault-plugin/pkg/utils"
+	"github.com/cyberark/conjur-api-go/conjurapi"
 )
 
 var fetchAllMaxSecrets = 1000
 
-type CyberArkSecretsManager struct{
-	client *conjurapi.Client
+type ConjurAPIClient interface {
+	Resources(filter *conjurapi.ResourceFilter) ([]map[string]interface{}, error)
+	RetrieveBatchSecrets(ids []string) (map[string][]byte, error)
+	RetrieveSecret(path string) ([]byte, error)
+	RetrieveSecretWithVersion(path string, version int) ([]byte, error)
 }
 
-func NewCyberArkSecretsManagerBackend(client *conjurapi.Client) *CyberArkSecretsManager {
+type CyberArkSecretsManager struct {
+	client ConjurAPIClient
+}
+
+func NewCyberArkSecretsManagerBackend(client ConjurAPIClient) *CyberArkSecretsManager {
 	return &CyberArkSecretsManager{
 		client: client,
 	}
@@ -69,55 +76,55 @@ func (csm *CyberArkSecretsManager) GetSecrets(path string, version string, annot
 	// Normalise secret IDs from batch secrets back to <variable_id>
 	var retrievedSecrets = make(map[string]interface{})
 	for id, secret := range retrievedSecretsByFullIDs {
-		retrievedSecrets[normaliseVariableId(id)] = string(secret)
+		retrievedSecrets[NormaliseVariableId(id)] = string(secret)
 		delete(retrievedSecretsByFullIDs, id)
 	}
 
 	if len(retrievedSecrets) == 0 {
-    	return nil, fmt.Errorf("no secrets found for given path")
-    }
+		return nil, fmt.Errorf("no secrets found for given path")
+	}
 	return retrievedSecrets, nil
 }
 
 // GetIndividualSecret will get the specific secret (placeholder) from the SM backend
 // This requires listing the secrets of the group to obtain the id, and then using that to grab the one secret's payload
 func (csm *CyberArkSecretsManager) GetIndividualSecret(kvpath, secretRef, version string, annotations map[string]string) (interface{}, error) {
-    var retrievedSecret interface{}
-    var err error
+	var retrievedSecret interface{}
+	var err error
 
-    secretPath := kvpath + "/" + secretRef
-    
-    // If version is specified, retrieve with version
-    if version != "" {
-        numberVersion, err := strconv.Atoi(version)
-        if err != nil {
-            return nil, fmt.Errorf("invalid version format: %s", version)
-        }
-        retrievedSecret, err = csm.client.RetrieveSecretWithVersion(secretPath, numberVersion)
-    } else {
-        // Otherwise, retrieve the latest version
-        retrievedSecret, err = csm.client.RetrieveSecret(secretPath)
-    }
-    
-    if err != nil {
-        return nil, fmt.Errorf("error retrieving secret from path '%s': %w", secretPath, err)
-    }
+	secretPath := kvpath + "/" + secretRef
 
-    // Convert to string if it's a byte slice (assuming that's the expected behavior)
-    switch v := retrievedSecret.(type) {
-    case []byte:
-        return string(v), nil
-    case string:
-        return v, nil
-    default:
-        return nil, fmt.Errorf("unexpected secret type: %T", v)
-    }
+	// If version is specified, retrieve with version
+	if version != "" {
+		numberVersion, err := strconv.Atoi(version)
+		if err != nil {
+			return nil, fmt.Errorf("invalid version format: %s", version)
+		}
+		retrievedSecret, err = csm.client.RetrieveSecretWithVersion(secretPath, numberVersion)
+	} else {
+		// Otherwise, retrieve the latest version
+		retrievedSecret, err = csm.client.RetrieveSecret(secretPath)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving secret from path '%s': %w", secretPath, err)
+	}
+
+	// Convert to string if it's a byte slice (assuming that's the expected behavior)
+	switch v := retrievedSecret.(type) {
+	case []byte:
+		return string(v), nil
+	case string:
+		return v, nil
+	default:
+		return nil, fmt.Errorf("unexpected secret type: %T", v)
+	}
 }
 
-func normaliseVariableId(fullVariableId string) string {
+func NormaliseVariableId(fullVariableId string) string {
 	variableIdParts := strings.SplitN(fullVariableId, ":", 3)
 	if len(variableIdParts) == 3 {
-		splitD := strings.Split(variableIdParts[2],"/")
+		splitD := strings.Split(variableIdParts[2], "/")
 		utils.VerboseToStdErr("secret variable :: %v", splitD[len(splitD)-1])
 		return splitD[len(splitD)-1]
 	}
